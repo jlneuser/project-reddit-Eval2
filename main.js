543d562dbc70d8b2bd5275816f87165704554b04
jlneuser <jlneuser@outlook.com>
2024-11-23 20:00:13 -0600

    final touches before submit

--- main.js
+++ main.js
@@ -22,31 +22,18 @@ rmButton.addEventListener('click', function () {
 
 const toggleButtons = document.querySelectorAll('.show-comments-btn');
 
-// Add event listeners to each button
+
 toggleButtons.forEach(button => {
   button.addEventListener('click', function () {
-    // Find the corresponding "new-comments" element (next sibling)
+
     const comments = this.parentElement.nextElementSibling;
 
-    // Toggle the visibility of the comments
+    
     if (comments.style.display === 'none' || comments.style.display === '') {
-      comments.style.display = 'block'; // Show comments
+      comments.style.display = 'block'; 
     } else {
-      comments.style.display = 'none'; // Hide comments
+      comments.style.display = 'none'; 
     }
   });
 });
 
-// submitButton.addEventListener('click', function () {
-
-//   const inputPost = document.getElementsByClassName('post')[0].value;
-//   const inputName = document.getElementsByClassName('name')[0].value;
-  
-//   const newSpan = document.createElement('span');
-//   newSpan.innerHTML = `${inputPost}, posted by ${inputName}`;
-//   const lineBreak = document.createElement('br');
-  
-//   const commentContainer = document.querySelector('.new-comments');
-//   commentContainer.appendChild(newSpan);
-//   commentContainer.appendChild(lineBreak);
-//   });
